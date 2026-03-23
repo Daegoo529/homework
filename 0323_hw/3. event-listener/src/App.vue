d5146fc7b00e23a788c070b8eb83d57af40bcbd5
daegoo529 <daegoo123@gmail.com>
2026-03-24 00:15:36 +0900

    0323_hw

---
+++ 0323_hw/3. event-listener/src/App.vue
@@ -0,0 +1,27 @@
+<!-- 
+v-if 컴포넌트에서 선언한 변수와 
+click 이벤트로 발생한 숫자를 비교
+
+
+-->
+
+<template>
+  <p>{{ counter }}</p>
+  <p v-if="counter < 5">5보다 작습니다.</p>
+  <p v-else>5와 같거나 큽니다.</p>
+  <button @click="counter++">클릭하면 숫자가 올라갑니다.</button>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  // Composition API
+  setup() {
+    let counter = ref(0);
+    return {
+      counter,
+    };
+  },
+};
+</script>
---
+++ 0323_hw/5. v-for/src/App.vue
@@ -0,0 +1,27 @@
+<!--
+전달받은 객체의 크기만큼 v-for 컴포넌트를 이용해 반복하여
+템플릿을 변경한다.
+
+-->
+
+<template>
+  <div style="width: 200px">
+    <ol>
+      <li v-for="item in items">{{ item }}</li>
+    </ol>
+  </div>
+</template>
+
+<script>
+import { reactive } from 'vue';
+
+export default {
+  // Composition API
+  setup() {
+    const items = reactive({ 1: '1번 객체', 2: '2번 객체', 3: '3번 객체' });
+    return {
+      items,
+    };
+  },
+};
+</script>
